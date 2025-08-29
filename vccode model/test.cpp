d9854e209b23e5c52eaa926d255632635922b699
Fall <ljq20060824@qq.com>
2025-08-29 21:32:09 +0800

    1

---
+++ vccode model/test.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+int main()
+{
+    int a=0;
+    printf("Hello World!\n");
+    printf("你好世界！\n");
+    scanf("%d",&a);
+    printf("a=%d\n",a);
+    return 0;
+}
